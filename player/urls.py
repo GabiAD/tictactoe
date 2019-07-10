77d531b466afac216dd88051818919869e63b531
GabiAD <gabriel.andrei96@yahoo.com>
2019-07-10 13:22:00 +0300

    Initial commit

---
+++ player/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView, LogoutView
+
+from player.views import home
+
+urlpatterns = [
+  url('home', home, name="player_home"),
+  url('login', LoginView.as_view(template_name="player/login_form.html"), name="player_login"),
+  url('logout', LogoutView.as_view(), name="player_logout"),
+]
---
+++ tictactoe/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+
+def welcome(request):
+  if request.user.is_authenticated:
+    return redirect("player_home")
+  else:
+    return render(request, "tictactoe/home.html")
