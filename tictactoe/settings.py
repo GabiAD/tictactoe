68135bd57cc965f7f256891580b4bfb10fcd9eb9
GabiAD <gabriel.andrei96@yahoo.com>
2019-07-11 17:39:26 +0300

    Added login and logout

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = "player_login"
+LOGIN_REDIRECT_URL = "player_home"
+LOGOUT_REDIRECT_URL = "tictactoe_welcome"
